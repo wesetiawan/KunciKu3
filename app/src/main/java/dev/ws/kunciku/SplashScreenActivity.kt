9b5137be77b0ef404c93b552f5bf35e06b73495f
Wawan Eka Setiyawan <wawaneka567@gmail.com>
2019-04-29 19:34:54 +0700

    adding welcome activity color style yoyo style

---
+++ app/src/main/java/dev/ws/kunciku/SplashScreenActivity.kt
@@ -0,0 +1,31 @@
+package dev.ws.kunciku
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        doAnimation()
+    }
+
+    private fun doAnimation(){
+        YoYo.with(Techniques.Tada)
+                .duration(1400)
+                .playOn(iv_logo)
+        Handler().postDelayed({
+            moveToWelcome()
+        },2400)
+    }
+    private fun moveToWelcome(){
+        startActivity(Intent(this,WelcomeActivity::class.java))
+    }
+}
