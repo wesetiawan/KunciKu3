cadfa00fa030d6ddc40b6440681d9aa9c824218e
Wawan Eka Setiyawan <wawaneka567@gmail.com>
2019-04-30 11:20:34 +0700

    more update

--- app/src/main/java/dev/ws/kunciku/MainActivity.java
+++ app/src/main/java/dev/ws/kunciku/MainActivity.java
@@ -1,16 +1,12 @@
 package dev.ws.kunciku;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
-
+import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +14,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
-
-
-
     }
-
-
     private void prepareTopNavBar(){
-        assert getSupportActionBar() != null;
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setDisplayShowHomeEnabled(true);
+        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.top_nav);
+        //getSupportActionBar().setElevation(0);
+        View view = getSupportActionBar().getCustomView();
+        TextView title = view.findViewById(R.id.title);
     }
 
     @Override
