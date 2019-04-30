d8b01ea6f9309467cb798d04aab80ebea72ba7a6
Wawan Eka Setiyawan <wawaneka567@gmail.com>
2019-04-30 07:25:36 +0700

    more update

--- app/src/main/java/dev/ws/kunciku/MainActivity.java
+++ app/src/main/java/dev/ws/kunciku/MainActivity.java
@@ -1,9 +1,19 @@
 package dev.ws.kunciku;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.LayoutInflaterCompat;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import com.mikepenz.iconics.context.IconicsContextWrapper;
+import com.mikepenz.iconics.context.IconicsLayoutInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +21,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar myToolbar = findViewById(R.id.navigation);
-        setSupportActionBar(myToolbar);
+
+
+
+    }
+
+
+    private void prepareTopNavBar(){
+        Toolbar top = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(top);
+        top.setTitle(getResources().getString(R.string.top_nav_title));
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(IconicsContextWrapper.wrap(newBase));
     }
 }
--- app/src/main/java/dev/ws/kunciku/WelcomeActivity.java
+++ app/src/main/java/dev/ws/kunciku/WelcomeActivity.java
@@ -2,13 +2,51 @@ package dev.ws.kunciku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        setUpButton();
+    }
+
+
+    private void setUpButton(){
+        Button login = findViewById(R.id.main_loginBtn);
+        login.setOnClickListener(this);
+        Button daftar = findViewById(R.id.main_regBtn);
+        daftar.setOnClickListener(this);
+        Button fb = findViewById(R.id.fb_button);
+        fb.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.main_loginBtn:
+                moveToMain();
+                break;
+
+            case R.id.main_regBtn:
+                moveToMain();
+                break;
+
+            case R.id.fb_button:
+                break;
+
+                default:
+                    break;
+        }
+    }
+
+    private void moveToMain(){
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
     }
 }
